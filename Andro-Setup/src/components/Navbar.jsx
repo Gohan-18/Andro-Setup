caec565e36cd0cd59a6abe3cf342bf7d56bc50b7
Prabhat <112978778+Gohan-18@users.noreply.github.com>
2023-03-07 16:52:06 +0530

    Navbar title designing

--- Andro-Setup/src/components/Navbar.jsx
+++ Andro-Setup/src/components/Navbar.jsx
@@ -1,20 +1,94 @@
-import { Box, Container, Typography } from '@mui/material';
-import React from 'react';
+import { Box, Container, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 const Navbar = () => {
+  const [activeFilter, setActiveFilter] = useState("Home");
+
+  useEffect(() => {
+    setActiveFilter("Home");
+  }, []);
+
   return (
     <>
-    <Box sx={{display: 'flex', }} >
-      <Box sx={{display: 'flex', }} >
-        {["Home", "Portfolio", "Contact"].map((item) => (
-          <Typography key={item} >
-            {item}
+      <Box
+        sx={{
+          display: "flex",
+          width: "100%",
+          justifyContent: "space-between",
+          alignItems: "center",
+          height: "100px",
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            height: "100%",
+            gap: 2,
+          }}
+        >
+          <Typography
+            sx={{ fontSize: "25px", fontWeight: "600", color: "#424242" }}
+          >
+            Andro/Setup
           </Typography>
-        ))}
+          <Box sx={{
+            backgroundColor: "#5A47AB",
+            color: "#FFFFFF",
+            py: '5px',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: 'auto',
+            px: '10px',
+            borderLeft:'1px dotted #666'
+          }} >
+          <LinkedInIcon sx={{height: '15px', width: '15px',}} />
+          <Typography
+            sx={{
+              ml: '4px',
+              textAlign: 'center',
+              fontSize: "8px",
+              color: "#ffffff",
+              lineHeight: '6px'
+            }}
+          >
+           Hire Me!!
+          </Typography>
+          </Box>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: 3,
+          }}
+        >
+          {["Home", "Portfolio", "Contact"].map((item) => {
+            let currentItm = activeFilter === item;
+            return (
+              <Typography
+                onClick={() => setActiveFilter(item)}
+                sx={{
+                  cursor: "pointer",
+                  color: !currentItm ? "#424242" : "#5A47AB",
+                  fontSize: "14px",
+                  fontWeight: "500",
+                  textTransform: "uppercase",
+                }}
+                key={item}
+              >
+                {item}
+              </Typography>
+            );
+          })}
+        </Box>
       </Box>
-    </Box>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
