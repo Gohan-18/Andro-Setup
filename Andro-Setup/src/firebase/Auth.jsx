695a14543b5811a67ab81d5c6f809715cc87b3a2
Prabhat <112978778+Gohan-18@users.noreply.github.com>
2023-03-12 13:28:04 +0530

    setting firebase for data access

---
+++ Andro-Setup/src/firebase/Auth.jsx
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getStorage, ref } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDyzrwfMb12WcUBCTThnInuYncb63Ok7nk",
+  authDomain: "andro-setup.firebaseapp.com",
+  projectId: "andro-setup",
+  storageBucket: "andro-setup.appspot.com",
+  messagingSenderId: "665815396425",
+  appId: "1:665815396425:web:38ca5ff0198ba9de018f98",
+  storageBucket: 'gs://andro-setup.appspot.com/'
+};
+
+const app = initializeApp(firebaseConfig);
+const storage = getStorage();
+
+const imagesRef = ref(storage, 'SetupPhotos');
