b00a79768633e48e0e88544c8989ed5f0b79b544
Prabhat <112978778+Gohan-18@users.noreply.github.com>
2023-03-15 20:29:08 +0530

    Added scrollbar styles

---
+++ Andro-Setup/src/components/HomeSetups.jsx
@@ -0,0 +1,16 @@
+import { Box, Typography } from '@mui/material'
+import React from 'react'
+
+const HomeSetups = () => {
+  return (
+    <>
+    <Box>
+        <Typography>
+            Setups
+        </Typography>
+    </Box>
+    </>
+  )
+}
+
+export default HomeSetups
--- Andro-Setup/src/pages/Home.jsx
+++ Andro-Setup/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../components/Header'
+import HomeSetups from '../components/HomeSetups'
 
 const Home = () => {
   return (
     <>
       <Header/>
+      <HomeSetups/>
     </>
   )
 }
