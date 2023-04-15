15e785bda2863fe5981fbdad52383b2c0a740ab8
Prabhat <pkjha187@gmail.com>
2023-04-15 22:42:55 +0530

    SingleSetup page ui half done

--- Andro-Setup/src/components/SingleSetup.jsx
+++ Andro-Setup/src/components/SingleSetup.jsx
@@ -1,17 +1,61 @@
-import { Box, Typography } from '@mui/material'
-import React from 'react'
-import { SetupState } from '../SetupContext';
+import { Box, CardMedia, Typography } from "@mui/material";
+import React from "react";
+import { SetupState } from "../SetupContext";
 
 const SingleSetup = () => {
-
   const { singleSetup, setSingleSetup } = SetupState();
-  console.log(singleSetup)
+  console.log(singleSetup);
+
+  const {
+    setupImage,
+    title,
+    id,
+    setup_id,
+    iconPack,
+    launcher,
+    uploadedDate,
+    wallpaper,
+    widgets,
+  } = singleSetup;
 
   return (
-    <Box sx={{mt: '100px'}} >
-      <Typography>Single setup</Typography>
-    </Box>
-  )
-}
+    <Box
+      sx={{
+        mt: "100px",
+        display: "flex",
+        alignItems: "start",
+        justifyContent: "space-between",
+        // flexDirection: "column",
+        width: '100%',
+        pt: '30px'
+      }}
+    >
+      {/* <Typography>Single setup</Typography> */}
+      <Box sx={{width: '50%'}} >
+      <CardMedia
+        component="img"
+        image={setupImage?.url}
+        alt={title}
+        sx={{
+          // maxHeight: '300px',
+          width: "500px",
+          height: "500px",
+          // width: "100%",
+          // height: "100%",
+          objectFit: "cover",
+          backgroundColor: "#f8f9fa",
+          borderRadius: '20px'
+        }}
+      />
+      </Box>
 
-export default SingleSetup
+      <Box 
+        sx={{display: 'flex', alignItems: 'start', justifyContent: 'start', flexDirection: 'column', width: '50%' }} >
+        <Typography>{title} </Typography>
+      </Box>
+
+    </Box>
+  );
+};
+
+export default SingleSetup;
