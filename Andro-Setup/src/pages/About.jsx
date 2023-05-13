9404ce53779f342fe6dbabe02af26df172c12071
Prabhat <pkjha187@gmail.com>
2023-05-13 21:58:07 +0530

    inor ui change about page

--- Andro-Setup/src/pages/About.jsx
+++ Andro-Setup/src/pages/About.jsx
@@ -1,28 +1,32 @@
-import { Box, Typography } from '@mui/material'
-import React, { useEffect } from 'react'
-import { SetupState } from '../SetupContext';
+import { Box, Typography } from "@mui/material";
+import React, { useEffect } from "react";
+import { SetupState } from "../SetupContext";
 
 function About() {
-
-  const { activeFilter, setActiveFilter, navbarState, setNavbarState } = SetupState();
+  const { activeFilter, setActiveFilter, navbarState, setNavbarState } =
+    SetupState();
 
   useEffect(() => {
-    setActiveFilter('About');
-    setNavbarState(false)
-  }, [])
-  
+    setActiveFilter("About");
+    setNavbarState(false);
+  }, []);
 
   return (
     <>
-    <Box sx={{pt: '100px'}} >
-        <Box>
+      <Box sx={{ pt: "100px" }}>
+        <Box sx={{py: '20px'}} >
           <Typography>
-            So, you have been using same boring home screen that you get shipped as default from your android device manufacturer, or what your kid set up for you, by basic changes like changinf the dafault wallpaper, and you want to make it look different, something of your taste. But clueless how to go by that!!? Look no further than Andro/Setup.
+            So, you have been using same boring home screen that you get shipped
+            as default from your android device manufacturer, or what your kid
+            set up for you, by basic changes like changing the dafault
+            wallpaper, and you want to make it look different, something of your
+            taste. But clueless how to go by that!!? Look no further than
+            Andro/Setup.
           </Typography>
         </Box>
-    </Box>
+      </Box>
     </>
-  )
+  );
 }
 
-export default About
+export default About;
